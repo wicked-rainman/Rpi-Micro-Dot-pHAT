6e74743e2c5699c9ba3fbc3cd5bbf9cbc6d1c68b
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-01-05 08:59:58 +0000

    Update bright.c

--- bright.c
+++ bright.c
@@ -1,19 +1,28 @@
 //-------------------------------------------------------------------------
 // bright - Change the light intensity for the display
 // 0x19 is the pulse width modulation register in the IS31FL3730 chip.
+// 0x0d is the Lighting Effect register (current)
 // There are 3 chips on board, 0x61, 0x62 and 0x63
 // Valid values for PWM are not obvious, but through trial and error, 0-7
 // seem to work. Anything after 8 seems to make no difference.
+// Valid values for current :
+//      0 = 40ma ..... 7 =75ma
+//      8 = 5ma .......14 =35ma
 //
 // WR
 //-------------------------------------------------------------------------
-void bright(int fd, int val) {
-        uint8_t buf[2],k,chip_addr=0x61;
+void bright(int fd, int cur, int pwm) {
+        uint8_t buf[2],current[2],k,chip_addr=0x61;
         if(fd<0) return;                                //Device hasn't been opened
+        if(cur>7) cur=7;                                //Don't go over 7
+        if(pwm>7) pwm=7;                                //A guess
         buf[0]=0x19;                                    //Brightness PWM reg
-        buf[1]=val;                                     //Brightness value
+        buf[1]=pwm;                                     //pwm value
+        current[0]=0x0d;                                //Lighting effect reg
+        current[1]=cur;                                 //0x00 is 40ma 0x07 is 75ma
         for(k=0;k<3;k++) {                              //Deal with the 3 chips
                 ioctl(fd, I2C_SLAVE,chip_addr+k);       //Select the chip
-                write(fd,buf,2);                        //Write the register
+                write(fd,current,2);                    //Write current value
+                write(fd,buf,2);                        //Write pwm value
         }
 }
